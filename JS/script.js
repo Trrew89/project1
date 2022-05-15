57c51541794340623a81220e4c331d0e366f407c
Sveatoslav <sveatoslavbodrus@gmail.com>
2022-05-15 11:05:04 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,28 @@
+"use strict";
+/*
+const category = 'toys';
+console.log(`someurl.com/${category}/5`);
+
+const user = 'Ivan';
+
+alert (`Привет, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+    ++incr;
+    --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+*/
+console.log(2+2*2 !== '6');
+
+const isChecked = false,
+       isClosed = false;
+
+console.log(isChecked || !isClosed);
